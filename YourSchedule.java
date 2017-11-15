f261c3afb6e72826ae3baf56e924db9c77409d01
nhunguyen76 <ntqn960607@gmail.com>
2017-11-15 11:19:54 +0700

    Bai 13

---
+++ YourSchedule.java
@@ -0,0 +1,20 @@
+public class YourSchedule
+{
+    public static void main( String[] args )
+    {
+        String course1,course2,course3,teacher1,teacher2,teacher3;
+        course1="VDK";
+        course2="TTCN";
+        course3="Java";
+        teacher1="Mr.Duy";
+        teacher2="Mr.Hoang";
+        teacher3="Mr.Ha";
+
+
+        System.out.println("+------------------------------+");
+        System.out.printf("| 1 | %11s | %10s |\n",course1,teacher1);
+        System.out.printf("| 2 | %11s | %10s |\n",course2,teacher2);
+        System.out.printf("| 3 | %11s | %10s |\n",course3,teacher3);
+        System.out.println("+------------------------------+");
+    }
+}
