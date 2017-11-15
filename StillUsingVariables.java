26ea3eb7fffff46ccf2798a7398c74ed18d1e2b8
nhunguyen76 <ntqn960607@gmail.com>
2017-11-15 11:17:02 +0700

    Bai 12

---
+++ StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class StillUsingVariables
+{
+    public static void main( String[] args )
+    {
+        String name="Nhu";
+        int year=2019;
+
+        System.out.println("My name is "+name+ " and I'll graduate in "+year);
+    }
+}
