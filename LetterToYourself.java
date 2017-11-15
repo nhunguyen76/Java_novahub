f7685fbc2591fb855aa6357ebb6fca9c42138739
nhunguyen76 <ntqn960607@gmail.com>
2017-11-15 11:04:53 +0700

    Bai

---
+++ LetterToYourself.java
@@ -0,0 +1,12 @@
+public class LetterToYourself{
+	public static void main (String[] args){
+		System.out.println("+-----------------------------------------+");
+		System.out.println("|                                    #### |");
+		System.out.println("|                                    #### |");
+		System.out.println("|                                    #### |");
+		System.out.println("|                                         |");
+		System.out.println("|                                         |");
+		System.out.println("|                         Nhu Nguyen      |");
+		System.out.println("|                            DN           |");
+	}
+}
