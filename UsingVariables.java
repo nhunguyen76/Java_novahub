1f8e70879c1c4f240ca7e4975a1ec3060433c65b
nhunguyen76 <ntqn960607@gmail.com>
2017-11-15 11:15:48 +0700

    Bai 11

---
+++ UsingVariables.java
@@ -0,0 +1,13 @@
+public class UsingVariables
+{
+    public static void main( String[] args )
+    {
+        int Var_1=113;
+        double Var_2=2.71828;
+        String Var_3="Computer Science";
+
+        System.out.println("This is room # "+Var_1);
+        System.out.println("e is close to "+Var_2);
+        System.out.println("I am leaning a bit about "+Var_3);
+    }
+}
